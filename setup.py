94d093c99801b5eaf34185517ae5378a8273b368
Slavi Pantaleev <s.pantaleev@gmail.com>
2017-02-18 08:11:51 +0200

    Add Python 3.6 to list of supported versions

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
         "Development Status :: 4 - Beta",
         "Environment :: Console",
         "Intended Audience :: Developers",
